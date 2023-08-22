9eb36838fddc6128ea57a0679d5cc0c61dbace45
f4rcy <110320416+f4rcy@users.noreply.github.com>
2023-08-22 17:35:13 -0500

    Update Buscador_Maquinas.sh

--- Buscador_Maquinas.sh
+++ Buscador_Maquinas.sh
@@ -24,51 +24,51 @@ respuesta_no_valida="\n${redColour}[!]${endColour} ${grayColour}Esa respuesta no
 
 #funciones
 function help_panel(){
-	echo -e "\n${yellowColour}[x]${endColour} uso: "
-	echo -e "\t${purpleColour}u:${endColour} Descargar o actualizar archivos necesarios."
-	echo -e "\t${purpleColour}m:${endColour} Buscar una maquina."
-	echo -e "\t${purpleColour}i:${endColour} Buscar por direción IP."
-	echo -e "\t${purpleColour}o:${endColour} Buscar por Sistema operativo."
-	echo -e "\t${purpleColour}d:${endColour} Buscar por nivel de dificultad."
-	echo -e "\t${purpleColour}s:${endColour} Buscar por skills. ${yellowColour}(colocalas entre doble comillas para evitar errores)${endColour}"
-	echo -e "\t${purpleColour}l:${endColour} Obtener link de youtube de una maquina."
-	echo -e "\t${purpleColour}h:${endColour} Mostrar este panel de ayuda."
+	echo -e "\n${yellowColour}[x]${endColour} ${grayColour} Uso:${endColour}"
+        echo -e "\t${purpleColour}u:${endColour} ${grayColour} Descargar o actualizar archivos necesarios.${endColour}"
+        echo -e "\t${purpleColour}m:${endColour} ${grayColour} Buscar una maquina.${endColour}"
+        echo -e "\t${purpleColour}i:${endColour} ${grayColour} Buscar por direción IP.${endColour}"
+        echo -e "\t${purpleColour}o:${endColour} ${grayColour} Buscar por Sistema operativo.${endColour}"
+        echo -e "\t${purpleColour}d:${endColour} ${grayColour} Buscar por nivel de dificultad.${endColour}"
+        echo -e "\t${purpleColour}s:${endColour} ${grayColour} Buscar por skills.${endColour}${yellowColour} (colocalas entre doble comillas para evitar errores)${endColour}"
+        echo -e "\t${purpleColour}l:${endColour} ${grayColour} Obtener link de youtube de una maquina.${endColour}"
+        echo -e "\t${purpleColour}h:${endColour} ${grayColour} Mostrar este panel de ayuda.${endColour}\n"
 }
 
 function update(){
-        tput civis
-        if [ ! -f bundle.js ]; then
-        echo -e "\n[x] Descargando archivos necesarios.."
-        curl -s $url > budle.js
-        js-beautify budle.js | sponge bundle.js
-        echo -e "\n[x] Archivos descargados :D"
-        else
-        curl -s $url > budle_temp.js
-        js-beautify budle_temp.js | sponge bundle_temp.js
-        md5=$(md5sum bundle.js | awk '{print $1}')
-        md5_temp=$(md5sum bundle_temp.js | awk '{print $1}')
-
-        if [ "$md5_temp" == "$md5_temp" ]; then
-        echo -e "\n[x] No hay actualizaciones pendientes"
-        rm -r bundle_temp.js
-        else
-        echo -e "\n[x] Actualizando.."
-        rm bundle.js && mv bundle_temp.js bundle.js
-        fi
-        tput cnorm
-        fi
+	tput civis
+	if [ ! -f bundle.js ]; then
+	echo -e "\n[x] ${grayColour}Descargando archivos necesarios..${endColour}\n"
+	curl -s $url > bundle.js
+        js-beautify bundle.js | sponge bundle.js
+	echo -e "\n[x]${grayColour} Archivos descargados ${endColour}:D\n"
+	else
+	curl -s $url > bundle_temp.js
+        js-beautify bundle_temp.js | sponge bundle_temp.js
+	md5=$(md5sum bundle.js | awk '{print $1}')
+	md5_temp=$(md5sum bundle_temp.js | awk '{print $1}')
+	
+	if [ "$md5_temp" == "$md5_temp" ]; then
+	echo -e "\n[x]${grayColour} No hay actualizaciones pendientes${endColour}\n"
+	rm -r bundle_temp.js
+	else
+	echo -e "\n[x]${grayColour} Actualizando..${endColour}\n"
+	rm bundle.js && mv bundle_temp.js bundle.js
+	fi
+	tput cnorm
+	fi
 }
 
 function searchmachine(){
         name_machine="$1"
-	echo -e "\n${greenColour}[-]${endColour} Listando propiedades de la maquina ${purpleColour}$name_machine${endColour}: \n"
+	echo -e "\n${greenColour}[-]${endColour} ${grayColour} Listando propiedades de la maquina ${purpleColour}$name_machine${endColour}: \n"
 	cat bundle.js | awk "/name: \"$name_machine\"/,/resuelta/" | grep -vE "id|sku|resuelta" | tr -d '"' | tr -d ',' | sed 's/ *//'
 }
 
 function searchIP(){
 	dir_ip="$1"
 	name_machine=$(cat bundle.js | grep "ip: \"$dir_ip\"" -B 5 | grep "name: " | tr -d ',' | tr -d '"' | awk 'NF{print $NF}')
-	echo -e "\n${greenColour}[-]${endColour} El nombre de la maquina de la IP ${blueColour}$dir_ip es: ${purpleColour}$name_machine${endColour}\n"
+	echo -e "\n${greenColour}[-]${endColour}${grayColour} El nombre de la maquina de la IP ${blueColour}$dir_ip${grayColour} es: ${purpleColour}$name_machine${endColour}\n"
 }
 
 function searchlink(){
@@ -76,9 +76,9 @@ function searchlink(){
 
 	if [ "name_machine" ]; then
 	link="$(cat bundle.js | awk "/name: \"$name_machine\"/,/resuelta/" | grep -vE "id|sku|resuelta" | tr -d '"' | tr -d ',' | sed 's/ *//' | grep "youtube" | awk 'NF{print $NF}')"
-	echo -e "\n${greenColour}[-]${endColour} El link de youtube de la maquina ${purpleColour}$name_machine${endColour} es: ${blueColour}$link${endColour}"
+	echo -e "\n${greenColour}[-]${endColour}${grayColour} El link de youtube de la maquina ${purpleColour}$name_machine${endColour}${grayColour} es: ${blueColour}$link${endColour}"
 	else
-	echo -e "\n${redColour}[x]${endColour} La maquina no existe"
+	echo -e "\n${redColour}[x]${endColour}${grayColour} La maquina no existe${endColour}"
 	fi
 }
 
@@ -87,7 +87,7 @@ function searchdif(){
 	name_machine=$(cat bundle.js | grep "dificultad: \"$dificultad\"" -B 5 | grep "name: " | tr -d ',' | tr -d '"' | awk 'NF{print $NF}' | column)
 
 	if [ "name_machine" ]; then
-	echo -e "\n${greenColour}[-]${endColour} Las maquinas de dificultad ${blueColour}$dificultad${endColour} son:\n"
+	echo -e "\n${greenColour}[-]${grayColour} Las maquinas de dificultad ${blueColour}$dificultad${grayColour} son: ${endColour}\n"
 	cat bundle.js | grep "dificultad: \"$dificultad\"" -B 5 | grep "name: " | tr -d ',' | tr -d '"' | awk 'NF{print $NF}' | column
 	else
 	echo -e "\n${redColour}[x]${endColour} ${grayColour}No existe esa dificultad${endColour}\n"
@@ -99,7 +99,7 @@ function searchOs(){
 	name_machine=$(cat bundle.js | grep "so: \"$os\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d ',' | tr -d '"' | column)
 
 	if  [ "name_machine" ]; then
-	echo -e "\n${greenColour}[-]${endColour} Las maquinas con sistema operativo ${blueColour}$os${endColour} son: \n"
+	echo -e "\n${greenColour}[-]${endColour}${grayColour} Las maquinas con sistema operativo ${blueColour}$os${grayColour} son:${endColour}\n"
 	cat bundle.js | grep "so: \"$os\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d ',' | tr -d '"' | column
 	else
 	echo -e "\n${redColour}[x]${endColour} ${grayColour}No existe ese sistema operativo${endColour}\n"
@@ -112,7 +112,7 @@ function searchdif_os(){
 	name_machine=$(cat bundle.js | grep "so: \"$os\"" -C 4 | grep "dificultad: \"$dificultad\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d ',' | tr -d '"' | column)
 
 	if [ "name_machine" ]; then
-	echo -e "\n${greenColour}[-]${endColour} Listando maquinas con dificultad ${blueColour}$dificultad${endColour} y sistema operativo ${purpleColour}$os${endColour}..."
+	echo -e "\n${greenColour}[-]${grayColour} Listando maquinas con dificultad ${blueColour}$dificultad${grayColour} y sistema operativo ${purpleColour}$os${endColour}..."
 	cat bundle.js | grep "so: \"$os\"" -C 4 | grep "dificultad: \"$dificultad\"" -B 5 | grep "name: " | awk 'NF{print $NF}' | tr -d ',' | tr -d '"' | column
 	else
 	echo -e "\n${redColour}[x]${endColour} ${grayColour}No existe esa dificultad o sistema operativo${endColour}\n"
@@ -124,7 +124,7 @@ function searchskill(){
 	name_machine=$(cat bundle.js | grep "skills:" -B 6 | grep "$skill" -i -B 6 | grep "name: " | tr -d ',' | tr -d '"' | awk 'NF{print $NF}' | column)
 
 	if [ "name_machine" ]; then
-	echo -e "\n${greenColour}[-]${endColour} Las maquinas con skill ${blueColour}$skill${endColour} son:\n"
+	echo -e "\n${greenColour}[-]${grayColour} Las maquinas con skill ${blueColour}$skill${grayColour} son:${endColour}\n"
 	cat bundle.js | grep "skills:" -B 6 | grep "$skill" -i -B 6 | grep "name: " | tr -d ',' | tr -d '"' | awk 'NF{print $NF}' | column
 	else
 	echo -e "\n${redColour}[x]${endColour} ${grayColour}No existe esa skill${endColour}\n"
